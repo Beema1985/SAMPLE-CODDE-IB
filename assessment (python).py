3298b2a9fd0f2bc285288abd25eccf6e20101663
namanjain007492 <naman@jainvidyalaya.in>
2025-07-07 09:54:39 +0530

    Add files via upload

---
+++ assessment (python).py
@@ -0,0 +1,10 @@
+#Assesment-1
+##start = int(input("Enter the starting number: "))
+##end = int(input("Enter the ending number: "))
+##for num in range(start, end + 1):
+##    if num % 11 == 0:
+##        print(num)
+
+#Assesment 2
+##for i in range(1,41,3):
+##    print(i,end=',')
